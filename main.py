a7546198773678665972af2040a71f0a880899af
loophac <dutytocountry@gmail.com>
2020-02-07 15:46:42 +0000

    -cleaned up terminal by adding ">" for all user input dialog.

--- main.py
+++ main.py
@@ -6,42 +6,42 @@ def mainInterface():
   print("RQL v1.0.1-dev")
   command = (input(">"))
   if command == "create":
-    nstar_system = input("Name for new Star:") 
+    nstar_system = input("Name for new Star:\n>") 
     d = shelve.open(nstar_system + ".star")
     mainInterface()
   elif command == "store":
-    sStarSystem = input("What Star do you want to store in?")
-    d = shelve.open(sStarSystem)
-    sPlanet = input("Which planet?")
-    dataTW = input("What to store:")
+    sStarSystem = input("What Star do you want to store in?\n>")
+    d = shelve.open(sStarSystem + ".star")
+    sPlanet = input("Which planet?\n>")
+    dataTW = input("What to store\n>")
     d[sPlanet] = dataTW
     d.close
     mainInterface()
   elif command == "view":
-    vStarSystem = input("What Star to view?") 
+    vStarSystem = input("What Star to view?\n>") 
     d = shelve.open(vStarSystem + ".star")
-    vPlanet = input("Planet to view?")
+    vPlanet = input("Planet to view?\n>")
     vdata = d[vPlanet]
     print(vdata)
     input("Press Enter to Continue...")
     mainInterface()
   elif command == "index planet":
-    iStarSystem = input("What Star to view?") 
+    iStarSystem = input("What Star to view?\n>") 
     d = shelve.open(iStarSystem + ".star") 
     klist = list(d.keys())
     print(klist)
     input("Press Enter to Continue...")
     mainInterface()
   elif command == "delete":
-    dStarSystem = input("What Star to open?") 
+    dStarSystem = input("What Star to open?\n>") 
     d = shelve.open(dStarSystem + ".star")
-    dKey = input("What Planet to delete?")
+    dKey = input("What Planet to delete?\n>")
     del d[dKey]
     print("Deleted.")
     input("Press Enter to Continue...")
     mainInterface()
   elif command == "where":
-    qStar = input("What Star to Query?")
+    qStar = input("What Star to Query?\n>")
     d = shelve.open(qStar + ".star")
     qString = input("Search term:")
     qList = list(d.keys())
