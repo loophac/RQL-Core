6fc6f75925b4a79c2003bb740764277bd8e280de
loophac <dutytocountry@gmail.com>
2020-02-07 15:23:03 +0000

    -Added semi-functional "where" function -Changed index to index planets in a star -Added index to see how many stars there are

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import shelve
+import os
+import glob
 
 def mainInterface():
-  print("RQL v1.0.0.")
+  print("RQL v1.0.1-dev")
   command = (input(">"))
   if command == "create":
-    nstar_system = input("Name for new Star System:") 
-    d = shelve.open(nstar_system)
+    nstar_system = input("Name for new Star:") 
+    d = shelve.open(nstar_system + ".star")
     mainInterface()
   elif command == "store":
-    sStarSystem = input("What Star System do you want to store in?")
+    sStarSystem = input("What Star do you want to store in?")
     d = shelve.open(sStarSystem)
     sPlanet = input("Which planet?")
     dataTW = input("What to store:")
@@ -16,28 +18,46 @@ def mainInterface():
     d.close
     mainInterface()
   elif command == "view":
-    vStarSystem = input("What Star System to view?") 
-    d = shelve.open(vStarSystem)
+    vStarSystem = input("What Star to view?") 
+    d = shelve.open(vStarSystem + ".star")
     vPlanet = input("Planet to view?")
     vdata = d[vPlanet]
     print(vdata)
     input("Press Enter to Continue...")
     mainInterface()
-  elif command == "index":
-    iStarSystem = input("What Star System to view?") 
-    d = shelve.open(iStarSystem) 
+  elif command == "index planet":
+    iStarSystem = input("What Star to view?") 
+    d = shelve.open(iStarSystem + ".star") 
     klist = list(d.keys())
     print(klist)
     input("Press Enter to Continue...")
     mainInterface()
   elif command == "delete":
-    dStarSystem = input("What Star System to open?") 
-    d = shelve.open(dStarSystem)
+    dStarSystem = input("What Star to open?") 
+    d = shelve.open(dStarSystem + ".star")
     dKey = input("What Planet to delete?")
     del d[dKey]
     print("Deleted.")
     input("Press Enter to Continue...")
     mainInterface()
+  elif command == "where":
+    qStar = input("What Star to Query?")
+    d = shelve.open(qStar + ".star")
+    qString = input("Search term:")
+    qList = list(d.keys())
+    qPlanetResult = [s for s in qList if any(xs in s for xs in qString)]
+    #qData = d[qPlanetResult]  
+    #qDataResult = qData.find(qString) 
+    print(qPlanetResult)
+    input("Press Enter to Continue...")
+    mainInterface()
+  elif command == "index star":
+    sList = glob.glob("*.star")
+    print(sList)
+    input("Press Enter to Continue...")
+    mainInterface()
+
+
 
 
     
